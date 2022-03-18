bcc5e3fc9746717e7de714514cb234d769365c3d
Taurien <michel.a.c474@gmail.com>
2022-03-17 22:31:31 -0500

    author, books, book item, place models n' basic url get

--- books/models.py
+++ books/models.py
@@ -1,3 +1,43 @@
+from pyexpat import model
 from django.db import models
 
+
 # Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    author = models.CharField(max_length=50, default='Anon')
+    # author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    ISBN = models.CharField(max_length=50)
+    title = models.CharField(max_length=50)
+    subject = models.TextField()
+    publisher = models.CharField(max_length=50)
+    language = models.CharField(max_length=50)
+    number_of_pages = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+
+
+class BookItem(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+
+    barcode = models.CharField(max_length=50)
+    is_reference_only = models.BooleanField()
+    borrowed = models.DateTimeField()
+    due_date = models.DateTimeField()
+    price = models.IntegerField()
+    # format = models.
+    # status = models.
+    date_of_purchase = models.DateTimeField()
+    publication_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.status
--- books/views.py
+++ books/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Book, BookItem
+from .serializers import BookSerializer
+
 
 # Create your views here.
+class book_list(APIView):
+
+    def get(self, req):
+        books = Book.objects.all()
+        serializer = BookSerializer(books, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
